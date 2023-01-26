205b0809145f35f0de10caa1ee609c69a9d6e6a2
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-26 14:26:45 -0800

    WIP. render. clean up css

--- src/App.tsx
+++ src/App.tsx
@@ -11,25 +11,25 @@ function App() {
 
     return (
         <div className="App bg-green-500">
-            <div>
-                <a href="https://github.com/electron-vite/electron-vite-react" target="_blank">
-                    <img src="./electron-vite.svg" className="logo" alt="Electron + Vite logo" />
-                </a>
-            </div>
+            <a href="https://github.com/electron-vite/electron-vite-react" title="Click on the Electron + Vite logo to learn more" target="_blank">
+                <img src="./electron-vite.svg" className="logo" alt="Electron + Vite logo" />
+            </a>
+
             <h1>Electron + Vite + React</h1>
+
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
             </div>
+
             <p className="read-the-docs">
                 Click on the Electron + Vite logo to learn more
             </p>
+
             <div className="flex-center">
-                Place static files into the<code>/public</code> folder <img style={{ width: "5em" }} src={nodeLogo} alt="Node logo" />
+                Place static files into the<code>/public</code> folder 
+                <img style={{ width: "5em" }} src={nodeLogo} alt="Node logo" />
             </div>
         </div>
     );
