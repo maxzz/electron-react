ccaf95f64021d6426f5b4df8f0f07dd45bbe2987
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-29 14:24:22 -0800

    WIP. ini-pos. preserve last position

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -36,8 +36,8 @@ let win: BrowserWindow | null = null;
 
 // Here, you can also use other preload
 const preload = join(__dirname, '../preload/index.js');
-const url = process.env.VITE_DEV_SERVER_URL;
 const indexHtml = join(process.env.DIST, 'index.html');
+const url = process.env.VITE_DEV_SERVER_URL;
 
 async function createWindow() {
     win = new BrowserWindow({
@@ -115,4 +115,37 @@ ipcMain.handle('open-win', (_, arg) => {
     } else {
         childWindow.loadFile(indexHtml, { hash: arg });
     }
+});
+
+
+
+app.on('ready', function () {
+    const path = require("path");
+    const fs = require("fs");
+    const initPath = path.join(app.getPath('userData'), "init.json");
+
+    console.log(`app name ${app.getName()}`); // 'electron-vite-react'
+
+    //https://www.electronjs.org/docs/latest/api/app#appsetpathname-path
+    console.log(`data path ${initPath}`); // C:\Users\maxzz\AppData\Roaming\electron-vite-react\init.json
+
+    let data: any;
+    try {
+        data = JSON.parse(fs.readFileSync(initPath, 'utf8'));
+    }
+    catch (e) {
+    }
+
+    // // Create the browser window.
+    // win = new BrowserWindow((data && data.bounds) ? data.bounds : {width: 800, height: 600});
+    // ...
+
+    win.on("close", function () {
+        console.log('win', win);
+        const data = {
+            bounds: win.getBounds()
+        };
+        fs.writeFileSync(initPath, JSON.stringify(data));
+    });
+    // ...
 });
--- src/samples/node-api.ts
+++ src/samples/node-api.ts
@@ -3,12 +3,12 @@ import { lstat } from 'node:fs/promises';
 import { cwd } from 'node:process';
 
 ipcRenderer.on('main-process-message', (_event, ...args) => {
-    console.log('[Receive Main-process message]:', ...args);
+    console.log('tm:[Receive Main-process message]:', ...args);
 });
 
 lstat(cwd())
     .then(stats => {
-        console.log('[fs.lstat]', stats);
+        console.log('tm:[fs.lstat] on cwd()', stats);
     })
     .catch(err => {
         console.error(err);
