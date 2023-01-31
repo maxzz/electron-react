42d58bf50b08170b5e4e2e38183f2369aa1e2322
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-31 15:23:33 -0800

    WIP. renderer. update App layout. add tw animation

--- src/App.tsx
+++ src/App.tsx
@@ -12,32 +12,34 @@ export function App() {
     return (
         <div className="h-full grid grid-rows-[auto_1fr_auto]">
             <div className="bg-green-600">Header</div>
-            
-            <div className="p-4 bg-green-500">
+
+            <div className="p-4 flex flex-col space-y-4 bg-green-500">
 
                 <div className="flex items-center space-x-2">
                     <a
+                        className="flex-none"
                         href="https://github.com/electron-vite/electron-vite-react"
                         title="Click on the Electron + Vite logo to learn more" target="_blank"
                     >
-                        <img className="w-8 h-8" src="./electron-vite.svg" alt="Electron + Vite logo" />
+                        <img className="w-8 h-8 animate-spin [animation-duration:8s]" src="./electron-vite.svg" alt="Electron + Vite logo" />
                     </a>
 
                     <h1 className="font-semibold">Electron+Vite+React</h1>
+
+                    <p className="pb-4 text-[0.55rem]">
+                        * Click on the Electron + Vite logo to learn more
+                    </p>
                 </div>
 
                 <button className="place-self-center px-3 py-2 border-slate-400 border rounded shadow" onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </button>
 
-                <p className="read-the-docs">
-                    Click on the Electron + Vite logo to learn more
-                </p>
-
-                <div className="flex items-center">
-                    Place static files into the <span className="px-1 font-semibold">/public</span> folder
-                    <img className="w-5 h-5 fill-red-500" src={nodeLogo} alt="Node logo" />
+                <div className="text-xs flex items-center">
+                    <span className="whitespace-nowrap">Place static files into the</span> <span className="px-1 font-semibold">/public</span> folder
+                    <img className="w-4 h-4 fill-red-500" src={nodeLogo} alt="Node logo" />
                 </div>
+
             </div>
             <div className="bg-green-600">Footer</div>
         </div>
