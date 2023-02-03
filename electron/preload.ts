f80d934f5da423249712c9cf7dabdc5494b162ce
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-03 14:56:38 -0800

    WIP. renderer. vite-api.d types shared for main and renderer

--- electron/preload.ts
+++ electron/preload.ts
@@ -7,7 +7,8 @@ console.log('ipcRenderer', ipcRenderer);
 contextBridge.exposeInMainWorld('tmApi', {
     sendNotification: (message) => {
         ipcRenderer.send('notify', message);
-    }
+    },
+    confirm,
 });
 
 //const { appendLoading, removeLoading } = useLoading();
@@ -18,3 +19,13 @@ contextBridge.exposeInMainWorld('tmApi', {
 // };
 
 // setTimeout(removeLoading, 4999);
+
+export async function confirm(option: tmApi.DialogOptions | string): Promise<void> {
+    const res = await ipcRenderer.invoke('confirm', option)
+    if (res.response === 1) {
+        throw {
+            message        : 'Dialog: Confirm Canceled',
+            checkboxChecked: res.checkboxChecked,
+        }
+    }
+}
