88b1d8cd307ae9dd2ad6aee6cb1818f4358702f1
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-03 18:53:07 -0800

    WIP. renderer. main.ts clean up

--- electron/main.ts
+++ electron/main.ts
@@ -1,27 +1,14 @@
-import { app, BrowserWindow, shell, ipcMain, Notification } from 'electron';
-import { release } from 'node:os';
 import { join } from 'node:path';
+import { release } from 'node:os';
+import { app, BrowserWindow, shell, ipcMain, Notification } from 'electron';
 import { getIniOptions, saveIniOptions } from './app/ini-options';
 
-// The built directory structure
-//
-// ├─┬ dist-electron
-// │ ├─┬ main
-// │ │ └── index.js    > Electron-Main
-// │ └─┬ preload
-// │   └── index.js    > Preload-Scripts
-// ├─┬ dist
-// │ └── index.html    > Electron-Renderer
-//
 process.env.DIST_ELECTRON = join(__dirname, '../');
 process.env.DIST = join(process.env.DIST_ELECTRON, '../dist');
 process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL ? join(process.env.DIST_ELECTRON, '../public') : process.env.DIST;
 
-// Disable GPU Acceleration for Windows 7
-if (release().startsWith('6.1')) app.disableHardwareAcceleration();
-
-// Set application name for Windows 10+ notifications
-if (process.platform === 'win32') app.setAppUserModelId(app.getName());
+if (release().startsWith('6.1')) app.disableHardwareAcceleration(); // Disable GPU Acceleration for Windows 7
+if (process.platform === 'win32') app.setAppUserModelId(app.getName()); // Set application name for Windows 10+ notifications
 
 if (!app.requestSingleInstanceLock()) {
     app.quit();
@@ -35,11 +22,9 @@ if (!app.requestSingleInstanceLock()) {
 
 let win: BrowserWindow | null = null;
 
-// Here, you can also use other preload
-// const preload = join(__dirname, '../preload/index.js');
 const preload = join(__dirname, './preload.js');
 const indexHtml = join(process.env.DIST, 'index.html');
-const url = process.env.VITE_DEV_SERVER_URL;
+const hmrServerUrl = process.env.VITE_DEV_SERVER_URL;
 
 async function createWindow() {
     const iniOptions = getIniOptions();
@@ -50,23 +35,13 @@ async function createWindow() {
         ...(iniOptions?.bounds),
         webPreferences: {
             preload,
-            // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-            // Consider using contextBridge.exposeInMainWorld
-            // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
             nodeIntegration: false,
-            contextIsolation: true,
-
-            // preload,
-            // // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-            // // Consider using contextBridge.exposeInMainWorld
-            // // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-            // nodeIntegration: true,
-            // contextIsolation: false,
+            contextIsolation: true, //https://www.electronjs.org/docs/latest/tutorial/context-isolation
         },
     });
 
-    if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
-        win.loadURL(url);
+    if (hmrServerUrl) { //https://github.com/electron-vite/electron-vite-vue/issues/298
+        win.loadURL(hmrServerUrl);
         iniOptions?.devTools && win.webContents.openDevTools(); // Open devTool if the app is not packaged
     } else {
         win.loadFile(indexHtml);
@@ -89,10 +64,6 @@ async function createWindow() {
         saveIniOptions(win);
     });
 }
-
-ipcMain.on('notify', (event, message) => {
-    new Notification({ title: 'My Noti', body: message }).show();
-});
 
 app.whenReady().then(createWindow);
 
@@ -120,19 +91,25 @@ app.on('activate', () => {
     }
 });
 
-// New window example arg: new windows url
-ipcMain.handle('open-win', (_, arg) => {
-    const childWindow = new BrowserWindow({
-        webPreferences: {
-            preload,
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
+// ipcMain handlers
 
-    if (process.env.VITE_DEV_SERVER_URL) {
-        childWindow.loadURL(`${url}#${arg}`);
-    } else {
-        childWindow.loadFile(indexHtml, { hash: arg });
-    }
+// New window example arg: new windows url
+// ipcMain.handle('open-win', (_, arg) => {
+//     const childWindow = new BrowserWindow({
+//         webPreferences: {
+//             preload,
+//             nodeIntegration: true,
+//             contextIsolation: false,
+//         },
+//     });
+
+//     if (process.env.VITE_DEV_SERVER_URL) {
+//         childWindow.loadURL(`${hmrServerUrl}#${arg}`);
+//     } else {
+//         childWindow.loadFile(indexHtml, { hash: arg });
+//     }
+// });
+
+ipcMain.on('notify', (_event, message) => {
+    new Notification({ title: 'My Noti', body: message }).show();
 });
