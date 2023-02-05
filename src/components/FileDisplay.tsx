0e68d679837b9427a51eb60449b145739f218521
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-04 18:02:30 -0800

    WIP. app. connect UIDropContainer

--- src/components/FileDisplay.tsx
+++ src/components/FileDisplay.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { UiDropContainer } from './UIDropContainer';
+import React, { useState } from 'react';
+import { atom } from 'jotai';
+import { UIDropContainer } from './UI/UIDropContainer';
 
 export function FileDisplay() {
+    const [activeAtom] = useState(atom(false))
     return (
         <>
-            <UiDropContainer
+            <UIDropContainer
                 className="px-4 py-3 text-center bg-slate-700/20 border-slate-400 border rounded shadow"
+                activeAtom={activeAtom}
                 onDropped={(files: FileList) => {
                     console.log('files', files);
 
                 }}
             >
-                drop
-            </UiDropContainer>
+                drop zone
+            </UIDropContainer>
         </>
     );
 }
