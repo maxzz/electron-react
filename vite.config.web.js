ed8eb22a83f1a38743a2183ad0df0304eba92602
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-05 15:10:52 -0800

    WIP. app. siwtch web dev vite config to js; still exit code 1

--- vite.config.web.js
+++ vite.config.web.js
@@ -5,16 +5,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
     return {
+        base: '',
         plugins: [
             react(),
         ],
-
         resolve: {
             alias: {
                 '@': path.join(__dirname, 'src')
             },
         },
-
         server: {
             port: 3000,
         },
