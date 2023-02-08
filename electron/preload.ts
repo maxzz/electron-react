589b4c2a25e6e05e21e6b4906b48836ff823d305
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-08 15:38:18 -0800

    WIP. app. type definitions clean up. 2

--- electron/preload.ts
+++ electron/preload.ts
@@ -2,20 +2,17 @@ import { contextBridge, ipcRenderer, IpcRendererEvent } from "electron";
 
 // vvv....................................................................... // this copy of preload-types.ts: cannot be imported wo/ bundler
 //
-enum ToMainKeys {
-    notify = 'notify',
-    invokeFilesContent = 'tm-invoke-files-content',
-    openFiles = 'tm-open-files',
+const ToMainKeys = {
+    notify: 'notify',
+    invokeFilesContent: 'tm-invoke-files-content',
+    openFiles: 'tm-open-files',
 };
 
-enum ToRendererKeys {
-    gotFilesContent = 'tm-got-files-content',
+const ToRendererKeys = {
+    gotFilesContent: 'tm-got-files-content',
 };
 //
 // ^^^.......................................................................
-
-// type ToMainKeys = 'notify' | 'tm-open-files';
-// type ToRendererKeys = 'tm-got-files-content';
 
 const api: TmApi = {
     sendNotification: (message: string) => {
