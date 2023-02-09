1fa939fc84605307ddc63a521d06d66aea65d6ec
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-09 15:04:43 -0800

    WIP. renderer. update loaded content render

--- src/components/FileDisplay.tsx
+++ src/components/FileDisplay.tsx
@@ -5,13 +5,17 @@ import { filesContentAtom } from '@/store';
 export function FileDisplay() {
     const filesContent = useAtomValue(filesContentAtom);
     return (
-        <>
-        <div className="">diplay</div>
-        {filesContent.files?.map(({path, cnt}, idx) => {
-            return (
-                <div className="" key={idx}>{path}</div>
-            )
-        })}
-        </>
+        <div className="border-slate-900/20 border rounded">
+            <div className="px-2 py-2 border-slate-900/20 border-b">Loaded content:</div>
+
+            {filesContent.files?.map(({ path, cnt }, idx) => {
+                return (
+                    <div className="text-xs" key={idx}>
+                        <div className="px-2 py-2 bg-slate-900/20">{path}</div>
+                        <div className="px-2 py-2 bg-slate-100/20">{cnt}</div>
+                    </div>
+                );
+            })}
+        </div>
     );
 }
