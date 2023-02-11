d680377dfb98075205ef2c870bbbb49bb4317460
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-11 15:52:55 -0800

    WIP. renderer. add SectionHeader component

--- src/components/FileDisplay.tsx
+++ src/components/FileDisplay.tsx
@@ -1,35 +1,42 @@
-import React, { useState } from 'react';
+import React, { HTMLAttributes, useState } from 'react';
 import { useAtomValue } from 'jotai';
 import { filesContentAtom } from '@/store';
+
+function SectionHeader({ children }: HTMLAttributes<HTMLElement>) {
+    return (
+        <div className="px-2 py-2 border-slate-900/20 border-b">
+            {children}
+        </div>
+    );
+}
 
 export function FileDisplay() {
     const filesContent = useAtomValue(filesContentAtom);
     return (<>
         <div className="border-slate-900/20 border rounded">
-            <div className="px-2 py-2 border-slate-900/20 border-b">Loaded content:</div>
+            <SectionHeader>Loaded content: {!filesContent.files?.length && ' Drop It Here'}</SectionHeader>
 
-            {filesContent.files?.map(({ path, cnt }, idx) => {
-                return (
-                    <div className="text-xs" key={path}>
-                        <div className="px-2 py-2 bg-slate-900/20">{path}</div>
-                        <textarea className="w-full px-2 py-2 bg-slate-100/20 smallscroll" rows={5} value={cnt} readOnly />
-                    </div>
-                );
-            })}
+            {filesContent.files?.map(({ path, cnt }, idx) => (
+                <div className="text-xs" key={path}>
+                    <div className="px-2 py-2 bg-slate-900/20">{path}</div>
+                    <textarea className="w-full px-2 py-2 bg-slate-100/20 smallscroll" rows={5} value={cnt} readOnly />
+                </div>
+            ))}
         </div>
 
         {!!filesContent.failed?.length && (
             <div className="border-slate-900/20 border rounded">
-                <div className="px-2 py-2 border-slate-900/20 border-b">Failed to Load:</div>
-                {filesContent.failed?.map(({ path, cnt }, idx) => {
-                    return (
-                        <div className="text-xs" key={path}>
-                            <div className="px-2 py-2 bg-slate-900/20">{path}</div>
-                            <textarea className="w-full px-2 py-2 bg-slate-100/20 smallscroll" rows={5} value={cnt} readOnly />
-                        </div>
-                    );
-                })}
+                <SectionHeader>Failed to Load:</SectionHeader>
+
+                {filesContent.failed?.map(({ path, cnt }, idx) => (
+                    <div className="text-xs" key={path}>
+                        <div className="px-2 py-2 bg-slate-900/20">{path}</div>
+                        <textarea className="w-full px-2 py-2 bg-slate-100/20 smallscroll" rows={5} value={cnt} readOnly />
+                    </div>
+                ))}
             </div>
         )}
     </>);
 }
+
+// TODO: clear list
