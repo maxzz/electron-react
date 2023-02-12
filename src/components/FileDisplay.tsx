ad4a6e5c6c3aa4ee50873410cbe76ae26b71539d
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-11 16:06:47 -0800

    WIP. renderer. add CardFilename component

--- src/components/FileDisplay.tsx
+++ src/components/FileDisplay.tsx
@@ -1,11 +1,19 @@
-import React, { HTMLAttributes, useState } from 'react';
+import React, { HTMLAttributes } from 'react';
 import { useAtomValue } from 'jotai';
 import { filesContentAtom } from '@/store';
 
-function SectionHeader({ children }: HTMLAttributes<HTMLElement>) {
+function SectionHeader({ children, ...rest }: HTMLAttributes<HTMLElement>) {
     return (
-        <div className="px-2 py-2 border-slate-900/20 border-b">
+        <div className="px-2 py-2 border-slate-900/20 border-b" {...rest}>
             {children}
+        </div>
+    );
+}
+
+function CardFilename({ path, ...rest }: { path: string; } & HTMLAttributes<HTMLElement>) {
+    return (
+        <div className="px-2 py-2 bg-slate-900/20" {...rest}>
+            {path}
         </div>
     );
 }
@@ -18,7 +26,7 @@ export function FileDisplay() {
 
             {filesContent.files?.map(({ path, cnt }, idx) => (
                 <div className="text-xs" key={path}>
-                    <div className="px-2 py-2 bg-slate-900/20">{path}</div>
+                    <CardFilename path={path} />
                     <textarea className="w-full px-2 py-2 bg-slate-100/20 smallscroll" rows={5} value={cnt} readOnly />
                 </div>
             ))}
@@ -30,7 +38,7 @@ export function FileDisplay() {
 
                 {filesContent.failed?.map(({ path, cnt }, idx) => (
                     <div className="text-xs" key={path}>
-                        <div className="px-2 py-2 bg-slate-900/20">{path}</div>
+                        <CardFilename path={path} />
                         <textarea className="w-full px-2 py-2 bg-slate-100/20 smallscroll" rows={5} value={cnt} readOnly />
                     </div>
                 ))}
