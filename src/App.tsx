7e3da4e6e4499d409169ae9295cf15355f68f21e
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-12 13:54:23 -0800

    WIP. ---------- add test input drop files w/ webkitdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { doInvokeLoadFilesAtom, mainApi } from './store';
 import { DocumentDrop } from './components/UI/DocumentDrop';
 import { Section2_Title } from './components/Section2_Title';
 import { FileContentViews } from './components/FileContentViews';
 import nodeLogo from "./assets/node.svg";
 import './App.scss';
+import { atom, useAtom } from 'jotai';
 
 export function App() {
     return (<>
@@ -21,13 +22,14 @@ export function App() {
                     {/* <TestState /> */}
                     {/* <TheRest /> */}
                     <FileContentViews />
+                    <TheDrop />
                 </div>
             </div>
 
             <div className="px-1 py-2 text-xs text-green-900 bg-green-600">Footer</div>
         </div>
 
-        <DocumentDrop doDroppedFilesAtom={doInvokeLoadFilesAtom} />
+        {/* <DocumentDrop doDroppedFilesAtom={doInvokeLoadFilesAtom} /> */}
     </>);
 }
 
@@ -62,6 +64,34 @@ function TheRest() {
         <div className="text-xs">
             Place static files into the <span className="font-semibold">/public</span> folder
             <img className="inline-block w-4 h-4 fill-red-500" src={nodeLogo} alt="Node logo" />
+        </div>
+    );
+}
+
+function TheDrop() {
+    const filesAtom = useState(atom<string[]>([]))[0];
+    const [files, setFiles] = useAtom(filesAtom);
+    const inputRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        inputRef.current?.setAttribute('webkitdirectory', '');
+    }, []);
+    return (
+        <div className="text-xs">
+            <input ref={inputRef} type="file" multiple
+                onChange={(event) => {
+                    const files = event.target.files;
+                    if (files) {
+                        console.log('input drop', files);
+
+                        // setFiles([...files].map((file) => file.webkitRelativePath))
+                        setFiles([...files].map((file) => JSON.stringify(file.name)));
+                    }
+                }}
+            />
+            {files.map((file, idx) => (
+                <div className="" key={idx}>{file}</div>
+            ))}
+
         </div>
     );
 }
