4cee41ee01d6023bf065689a43477a79e43f8c5c
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-12 17:58:43 -0800

    WIP. renderer. bug. add readAllDirectoryEntries() w/ Promise<FileSystemEntry[]>

--- src/utils/web-drop-utils.ts
+++ src/utils/web-drop-utils.ts
@@ -4,7 +4,7 @@ export type DropItem = {
     isDir?: boolean;
     item: FileSystemEntry;
 };
-
+/*
 function scanFiles(item: FileSystemEntry, rv: DropItem[]) {
     rv.push({
         name: item.name,
@@ -25,7 +25,7 @@ function scanFiles(item: FileSystemEntry, rv: DropItem[]) {
     }
 }
 
-export function getFilesAndDirsFromItems(items: DataTransferItem[]) {
+export function getFilesAndDirsFromItems(items: DataTransferItem[]): DropItem[] {
     const rv: DropItem[] = [];
 
     for (let i = 0; i < items.length; i++) {
@@ -37,6 +37,68 @@ export function getFilesAndDirsFromItems(items: DataTransferItem[]) {
     }
 
     console.log('rv', rv);
+
+    return rv;
+}
+*/
+
+// Adapted from https://github.com/sanjibnarzary/bodo_music_server/blob/main/resources/assets/js/utils/directoryReader.ts
+// Adapted from https://stackoverflow.com/a/53058574
+async function readEntriesPromise(directoryReader: FileSystemDirectoryReader): Promise<FileSystemEntry[]> {
+    return await new Promise((resolve, reject): void => {
+        directoryReader.readEntries(resolve, reject);
+    });
+}
+
+async function readAllDirectoryEntries(directoryReader: FileSystemDirectoryReader): Promise<FileSystemEntry[]> {
+    const entries: FileSystemEntry[] = [];
+    let readEntries = await readEntriesPromise(directoryReader);
+
+    while (readEntries.length > 0) {
+        entries.push(...readEntries);
+        readEntries = await readEntriesPromise(directoryReader);
+    }
+
+    return entries;
+}
+
+async function getAllFileEntries(dataTransferItemList: DataTransferItemList): Promise<FileSystemEntry[]> {
+    const fileEntries: FileSystemEntry[] = [];
+    const queue: FileSystemEntry[] = [];
+
+    for (let i = 0, length = dataTransferItemList.length; i < length; i++) {
+        queue.push(dataTransferItemList[i].webkitGetAsEntry()!);
+    }
+
+    while (queue.length > 0) {
+        const entry = queue.shift();
+
+        if (!entry) {
+            continue;
+        }
+
+        if (entry.isFile) {
+            fileEntries.push(entry);
+        } else if (entry.isDirectory) {
+            queue.push(...await readAllDirectoryEntries((entry as FileSystemDirectoryEntry).createReader()));
+        }
+    }
+
+    return fileEntries;
+}
+
+export function getFilesAndDirsFromItems(items: DataTransferItem[]): DropItem[] {
+    const rv: DropItem[] = [];
+
+    // for (let i = 0; i < items.length; i++) {
+    //     let item = items[i].webkitGetAsEntry();
+
+    //     if (item) {
+    //         scanFiles(item, rv);
+    //     }
+    // }
+
+    // console.log('rv', rv);
 
     return rv;
 }
