28eb49cd086299ef6a45a3285214e9095ca885ad
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-12 16:51:43 -0800

    WIP. renderer. add getFilesFromevent()

---
+++ src/utils/web-drop-utils.ts
@@ -0,0 +1,40 @@
+export type DropItem = {
+    name: string;
+    fullPath: string;
+    isDir?: boolean;
+    item: FileSystemEntry;
+};
+
+function scanFiles(item: FileSystemEntry, rv: DropItem[]) {
+    rv.push({
+        name: item.name,
+        fullPath: item.fullPath,
+        isDir: item.isDirectory,
+        item,
+    });
+
+    console.log('item', item);
+
+    if (item.isDirectory) {
+        let directoryReader = (item as FileSystemDirectoryEntry).createReader();
+        directoryReader.readEntries((entries) => {
+            entries.forEach((entry) => {
+                scanFiles(entry, rv);
+            });
+        });
+    }
+}
+
+export function getFilesFromevent(dataTransfer: DataTransfer) {
+    const items = dataTransfer.items;
+
+    const rv: DropItem[] = [];
+
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i].webkitGetAsEntry();
+
+        if (item) {
+            scanFiles(item, rv);
+        }
+    }
+}
