52025a1d6d8f60dcb0cb1bcd65e172ee20269949
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-12 17:13:20 -0800

    WIP. renderer. switch to event.dataTransfer instead of Files[]

--- src/store/drop-files.ts
+++ src/store/drop-files.ts
@@ -10,18 +10,20 @@ export type DoDroppedFilesAtom = typeof doInvokeLoadFilesAtom;
 
 export const doInvokeLoadFilesAtom = atom(
     null,
-    async (get, set, dropFiles: File[]) => {
+    async (get, set, dataTransfer: DataTransfer) => {
         let filesCnt: FileContent[];
 
         if (mainApi) {
+            const dropFiles: File[] = [...dataTransfer.files];
             const filenames = getElectronPathes(dropFiles);
             if (!filenames.length) { return; }
 
             filesCnt = await mainApi?.invokeFilesContent(filenames);
         } else {
+            const dropFiles: File[] = [...dataTransfer.files];
             filesCnt = await loadWebFilesContent(dropFiles);
         }
-        
+
         if (filesCnt) {
             set(filesContentAtom, filesCnt);
         }
