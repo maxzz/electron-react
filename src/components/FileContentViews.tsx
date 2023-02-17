3149e65fc82177b0ad49cfcb351f19e50b9c1b0a
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-17 15:22:29 -0800

    WIP. app. textarea custom resize corner w/ gradients. 3. diff. sizes test

--- src/components/FileContentViews.tsx
+++ src/components/FileContentViews.tsx
@@ -20,6 +20,21 @@ function ButtonClear() {
     );
 }
 
+const css: React.CSSProperties = {
+    border: '1px solid transparent',
+    backgroundRepeat: 'no-repeat',
+    backgroundImage: `linear-gradient(135deg, 
+    transparent 50%, 50%, #2a7137 54%, 54%, 
+    transparent 54%, 54%, #42bc58 58%, 58%, 
+
+    transparent 65%, 65%, #2a7137 70%, 70%, 
+    transparent 70%, 70%, #42bc58 74%, 74%, 
+
+    transparent 82%, 82%, #2a7137 87%, 87%,
+    transparent 87%, 87%, #42bc58 91%, 91%, 
+    transparent)`,
+};
+
 function CardFilename({ path, fullPath, failed, ...rest }: { path: string; fullPath: string; failed?: boolean; } & HTMLAttributes<HTMLElement>) {
     return (
         <div className={`px-2 py-2 ${failed ? 'bg-red-600' : 'bg-neutral-900/20'}`} {...rest}>
@@ -32,18 +47,9 @@ function CardFilename({ path, fullPath, failed, ...rest }: { path: string; fullP
                 {/* <IconFolderOpen className="w-5 h-5" /> */}
                 <div className="text-[.55rem]">{fullPath}</div>
             </div>
-            <div className="w-24 h-24 border-gray-900 border" style={{
-                backgroundImage: `linear-gradient(135deg, 
-                transparent 50%, 50%, #2a7137 54%, 54%, 
-                transparent 54%, 54%, #42bc58 58%, 58%, 
-
-                transparent 66%, 66%, #2a7137 70%, 70%, 
-                transparent 70%, 70%, #42bc58 74%, 74%, 
-
-                transparent 83%, 83%, #2a7137 87%, 87%,
-                transparent 87%, 87%, #42bc58 91%, 91%, 
-                transparent)`}}>
-            </div>
+            <div className="w-3 h-3 mt-4 border-gray-900" style={css}></div>
+            <div className="w-4 h-4 mt-4 border-gray-900" style={css}></div>
+            <div className="w-24 h-24 mt-4 border-gray-900" style={css}></div>
         </div>
     );
 }
