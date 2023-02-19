f795f424607809b9db165bf06d9578b93ebb4133
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-19 14:24:52 -0800

    WIP. renderer. Section1_WebMenu add live prototype

--- src/components/Section1_WebMenu.tsx
+++ src/components/Section1_WebMenu.tsx
@@ -8,10 +8,27 @@ export function Section1_WebMenu() {
             <button onClick={() => setOpen((v) => !v)}>
                 <IconMenu className="w-4 h-4 flex-none fill-current" />
             </button>
-            {open && <div className="absolute w-max py-2 text-green-900 bg-green-300 border-neutral-900/50 border rounded flex flex-col items-start space-y-1">
-                <button className="mx-1 px-4 py-1 hover:text-green-100 hover:bg-green-700 rounded-sm">Open File...</button>
-                <button className="mx-1 px-4 py-1 hover:text-green-100 hover:bg-green-700 rounded-sm">Open Folder...</button>
-            </div>}
+
+            {open &&
+                <ul className="absolute w-max py-2 text-green-900 bg-green-300 border-neutral-900/50 border rounded grid space-y-1">
+                    <li
+                        className="mx-1 px-4 py-1 hover:text-green-100 hover:bg-green-700 rounded-sm cursor-pointer"
+                        onClick={() => {
+                            setOpen((v) => !v);
+                        }}
+                    >
+                        <div className="">Open File...</div>
+                    </li>
+                    <li
+                        className="mx-1 px-4 py-1 hover:text-green-100 hover:bg-green-700 rounded-sm cursor-pointer"
+                        onClick={() => {
+                            setOpen((v) => !v);
+                        }}
+                    >
+                        <div className="">Open Folder...</div>
+                    </li>
+                </ul>
+            }
         </div>
     );
 }
