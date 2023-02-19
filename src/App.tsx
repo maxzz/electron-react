c116e5bd878122d4013bd4000b5414e5157792e6
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-19 13:59:39 -0800

    WIP. renderer. add Section3_Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { doDroppedFilesAtom, sendToMain } from './store';
+import { doDroppedFilesAtom } from './store';
 import { WorldToReactListener } from './store/WorldToReactListener';
 import { DocumentDrop } from './components/UI/DocumentDrop';
-import { Section2_Title } from './components/Section2_Title';
-import { FileContentViews } from './components/FileContentViews';
-import nodeLogo from "./assets/node.svg";
-import './App.scss';
-import { IconMenu } from './components/UI/UIIcons';
 import { Section1_Header } from './components/Section1_Header';
+import { Section2_Title } from './components/Section2_Title';
 import { Section2_SendNotification } from './components/Section2_SendNotification';
+import { FileContentViews } from './components/FileContentViews';
+import './App.scss';
+import { Section3_Footer } from './components/Section3_Footer';
 
 export function App() {
     return (<>
@@ -20,24 +19,15 @@ export function App() {
                         <Section2_Title />
                         <Section2_SendNotification />
                     </div>
-                    {/* <TheRest /> */}
+
                     <FileContentViews />
                 </div>
             </div>
 
-            <div className="px-1 py-2 text-xs text-green-900 bg-green-600">Footer</div>
+            <Section3_Footer />
         </div>
 
         <DocumentDrop doDroppedFilesAtom={doDroppedFilesAtom} />
         <WorldToReactListener />
     </>);
-}
-
-function TheRest() {
-    return (
-        <div className="text-xs">
-            Place static files into the <span className="font-semibold">/public</span> folder
-            <img className="inline-block w-4 h-4 fill-red-500" src={nodeLogo} alt="Node logo" />
-        </div>
-    );
 }
---
+++ src/components/Section3_Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Section3_Footer() {
+    return (
+        <div className="px-1 py-2 text-xs text-green-900 bg-green-600">Footer</div>
+    );
+}
