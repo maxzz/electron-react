b1a80ac73c03ffa08a8885e828c66df506b2e023
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-20 01:16:48 -0800

    WIP. Section1_WebMenu. open file and directory code clean up

--- src/components/Section1_WebMenu.tsx
+++ src/components/Section1_WebMenu.tsx
@@ -1,45 +1,23 @@
-import { doDialogFilesAtom } from '@/store';
-import { useSetAtom } from 'jotai';
 import React, { useState } from 'react';
+import { useSetAtom } from 'jotai';
+import { doDialogFilesAtom } from '@/store';
 import { IconMenu } from './UI/UIIcons';
 
-function FileInput({ openFolder }: { openFolder?: boolean; }) {
+function FileInput({ openFolder, onChangeDone }: { openFolder?: boolean; onChangeDone?: () => void; }) {
     const doDialogFiles = useSetAtom(doDialogFilesAtom);
-    const doDir = {
-        ...(openFolder && { webkitdirectory: '' })
-    };
+    const doDir = { ...(openFolder && { webkitdirectory: '' }) };
     return (
-        <input
-            type="file"
-            className="hidden1"
-            multiple
-            {...doDir}
-            value=""
-            onClick={(e) => {
-                //e.target.value = null;
-                console.log('-------------e click', e);
-            }}
+        <input className="hidden" type="file" multiple {...doDir}
             onChange={(event) => {
-                //e.preventDefault();
-                if (event.target.files) {
-                    doDialogFiles([...event.target.files]);
-                    for (const file of event.target.files) {
-                        console.log('-------------e change', file);
-                    }
-                }
+                event.target.files && doDialogFiles([...event.target.files]);
+                onChangeDone?.();
             }}
-        // onChange={(e) => {
-        //     //e.preventDefault();
-        //     console.log('-------------e change', e.target.files);
-        // }}
         />
     );
 }
 
 export function Section1_WebMenu() {
     const [open, setOpen] = useState(true);
-    console.log('11111111111');
-
     return (
         <div className="relative">
             <button onClick={() => setOpen((v) => !v)}>
@@ -49,32 +27,17 @@ export function Section1_WebMenu() {
             {open &&
                 <form>
                     <ul className="absolute w-max py-2 text-green-900 bg-green-300 border-neutral-900/50 border rounded grid space-y-1">
-                        <li
-                            className="mx-1 px-4 py-1 hover:text-green-100 hover:bg-green-700 rounded-sm cursor-pointer"
-                            onClick={() => {
-                                //setOpen((v) => !v);
-                            }}
-                        >
-                            <label className="cursor-pointer"
-                            // onChange={(e) => {
-                            //     //e.preventDefault();
-                            //     console.log('-------------e change', e);
-                            // }}
-                            >
-                                <FileInput />
+                        <li className="mx-1 px-4 py-1 hover:text-green-100 hover:bg-green-700 rounded-sm cursor-pointer">
+                            <label className="cursor-pointer">
+                                <FileInput onChangeDone={() => setOpen((v) => !v)} />
                                 <div className="">Open File...</div>
                             </label>
 
 
                         </li>
-                        <li
-                            className="mx-1 px-4 py-1 hover:text-green-100 hover:bg-green-700 rounded-sm cursor-pointer"
-                            onClick={() => {
-                                //setOpen((v) => !v);
-                            }}
-                        >
+                        <li className="mx-1 px-4 py-1 hover:text-green-100 hover:bg-green-700 rounded-sm cursor-pointer">
                             <label className="cursor-pointer">
-                                <FileInput openFolder={true} />
+                                <FileInput onChangeDone={() => setOpen((v) => !v)} openFolder={true} />
                                 <div className="">Open Folder...</div>
                             </label>
                         </li>
