e917b7d7d60e2ef9fb12f16333dcebf37d31db6b
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-14 15:34:35 -0800

    WIP. shadcn. split css and scss for tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import './index.scss';
+import './assets/styles/index.css';
+import './assets/styles/extra.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
