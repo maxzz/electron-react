a2ada4404d341b90ab8e1835eead9cb65fe90513
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-14 16:27:11 -0800

    WIP. dropdown menu. code review. 3. remove duplicates

--- src/components/1-header/3-menu.tsx
+++ src/components/1-header/3-menu.tsx
@@ -12,24 +12,28 @@ export function MenubarSeparator({ children, ...rest }: MenubarSeparatorProps) {
     );
 }
 
-const s1Classes = "px-3 py-1 text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-900";
-const s4Classes = "text-sm font-medium text-gray-700 dark:text-gray-100";
-const s6Classes = "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75";
-const s7Classes = "cursor-default radix-disabled:opacity-50 radix-disabled:cursor-not-allowed";
+const s1Classes = "\
+px-3 py-1 \
+text-sm font-medium \
+\
+text-gray-700 dark:text-gray-100 \
+hover:bg-gray-100 dark:hover:bg-gray-900 \
+\
+focus:outline-none \
+focus-visible:ring \
+focus-visible:ring-purple-500 \
+focus-visible:ring-opacity-75 \
+\
+radix-disabled:opacity-50 \
+radix-disabled:cursor-not-allowed \
+\
+rounded-md \
+cursor-default";
 
 type MenubarTriggerProps = Omit<Prim.MenubarTriggerProps & React.RefAttributes<HTMLButtonElement>, "className">;
 export function MenubarTrigger({ children, ...rest }: MenubarTriggerProps) {
     return (
-        <Prim.Trigger
-            className={classNames(
-                s1Classes, "px-4",
-                "radix-state-open:bg-gray-100 dark:radix-state-open:bg-gray-900",
-                s4Classes,
-                s6Classes,
-                s7Classes,
-            )}
-            {...rest}
-        >
+        <Prim.Trigger className={classNames(s1Classes, "px-4", "radix-state-open:bg-gray-100 dark:radix-state-open:bg-gray-900",)} {...rest}>
             {children}
         </Prim.Trigger>
     );
@@ -38,15 +42,7 @@ export function MenubarTrigger({ children, ...rest }: MenubarTriggerProps) {
 type MenubarSubTriggerProps = Omit<Prim.MenubarSubTriggerProps & React.RefAttributes<HTMLDivElement>, "className">;
 export function MenubarSubTrigger({ children, ...rest }: MenubarSubTriggerProps) {
     return (
-        <Prim.SubTrigger
-            className={classNames(
-                s1Classes,
-                s4Classes,
-                s6Classes,
-                s7Classes,
-            )}
-            {...rest}
-        >
+        <Prim.SubTrigger className={classNames(s1Classes,)} {...rest}>
             <div className="w-full flex justify-between items-center">
                 {children}
                 <ChevronRightIcon className="ml-4 -mr-1 text-gray-700 dark:text-gray-100 font-medium" />
@@ -58,16 +54,7 @@ export function MenubarSubTrigger({ children, ...rest }: MenubarSubTriggerProps)
 type MenubarItemProps = Omit<Prim.MenubarItemProps & React.RefAttributes<HTMLDivElement> & { shortcut?: string; }, "className">;
 export function MenubarItem({ children, shortcut, ...rest }: MenubarItemProps) {
     return (
-        <Prim.Item
-            className={classNames(
-                "w-full flex items-center justify-between",
-                s1Classes,
-                s4Classes,
-                s6Classes,
-                s7Classes,
-            )}
-            {...rest}
-        >
+        <Prim.Item className={classNames(s1Classes, "w-full flex items-center justify-between",)} {...rest}>
             <div className="w-full flex justify-between items-center">
                 {children}
                 {shortcut && (
@@ -83,20 +70,11 @@ export function MenubarItem({ children, shortcut, ...rest }: MenubarItemProps) {
 type MenubarCheckboxItemProps = Omit<Prim.MenubarCheckboxItemProps & React.RefAttributes<HTMLDivElement>, "className">;
 export function MenubarCheckboxItem({ children, ...rest }: MenubarCheckboxItemProps) {
     return (
-        <Prim.CheckboxItem
-            className={classNames(
-                "w-full flex items-center justify-between",
-                s1Classes,
-                s4Classes,
-                s6Classes,
-                s7Classes,
-            )}
-            {...rest}
-        >
+        <Prim.CheckboxItem className={classNames(s1Classes, "w-full flex items-center justify-between",)} {...rest}>
             <div className="flex items-center">
-                <div className="relative h-3.5 w-3.5 -ml-1">
+                <div className="relative -ml-1 size-3.5">
                     <Prim.ItemIndicator>
-                        <CheckIcon className="h-3.5 w-3.5 -ml-0.5 mr-1.5" />
+                        <CheckIcon className="-ml-0.5 mr-1.5 size-3.5" />
                     </Prim.ItemIndicator>
                 </div>
                 <div className="ml-1">{children}</div>
@@ -108,18 +86,9 @@ export function MenubarCheckboxItem({ children, ...rest }: MenubarCheckboxItemPr
 type MenubarRadioItemProps = Omit<Prim.MenubarRadioItemProps & React.RefAttributes<HTMLDivElement>, "className">;
 export function MenubarRadioItem({ children, ...rest }: MenubarRadioItemProps) {
     return (
-        <Prim.RadioItem
-            className={classNames(
-                s7Classes,
-                "w-full flex items-center justify-between",
-                s1Classes,
-                s4Classes,
-                s6Classes,
-            )}
-            {...rest}
-        >
+        <Prim.RadioItem className={classNames(s1Classes, "w-full flex items-center justify-between",)} {...rest}>
             <div className="flex items-center">
-                <div className="relative h-3.5 w-3.5 -ml-1">
+                <div className="relative -ml-1 size-3.5">
                     <Prim.ItemIndicator>
                         <DotFilledIcon className="absolute inset-0" />
                     </Prim.ItemIndicator>
