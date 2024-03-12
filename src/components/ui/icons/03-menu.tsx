6ddaabf172c02839ee05d3cf676397ccc2e35492
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-11 18:43:16 -0700

    WIP. menu. update icons layout

--- src/components/ui/icons/03-menu.tsx
+++ src/components/ui/icons/03-menu.tsx
@@ -4,8 +4,7 @@ import { HTMLAttributes } from "react";
 export function IconMenu({ className, ...rest }: HTMLAttributes<SVGElement>) {
     return (
         <svg className={classNames("fill-none stroke-current", className)} viewBox="0 0 24 24" {...rest}>
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z">
-            </path>
+            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
         </svg>
     );
 }
